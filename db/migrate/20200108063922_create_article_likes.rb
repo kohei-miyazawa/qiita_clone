412c70e99bd7f95a75c0bd779cf3d1ee8335e6e3
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-01-08 16:05:59 +0900

    各テーブルに外部キーの追加

--- db/migrate/20200108063922_create_article_likes.rb
+++ db/migrate/20200108063922_create_article_likes.rb
@@ -1,7 +1,7 @@
 class CreateArticleLikes < ActiveRecord::Migration[5.2]
   def change
     create_table :article_likes do |t|
-      t.refereces :user, foreign_key: true
+      t.references :user, foreign_key: true
       t.references :article, foreign_key: true
 
       t.timestamps
