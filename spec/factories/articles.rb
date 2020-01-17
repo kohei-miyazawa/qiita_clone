fb066a125b9d3ec40219b1f4b3b482503d1fff7e
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-01-17 14:15:48 +0900

    ArticleモデルのFactoryBot,testの実装

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    body { "MyText" }
+    title { Faker::Lorem.characters(number: Random.new.rand(1..100))}
+    body { Faker::Lorem.paragraph}
+    user
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系" do
+    context "body,titleが存在する場合" do
+      let(:article){build(:article)}
+      it "記事が投稿される" do
+        expect(article).to be_valid
+      end
+    end
+  end
+
+  describe "異常系" do
+    context "titleが存在しない場合" do
+      let(:article){build(:article,title:nil)}
+      it "エラーする(can't be blank)" do
+        article.valid?
+        expect(article.errors.messages[:title]).to include "can't be blank"
+      end
+    end
+  end
 end
