8b94e8e3fcf7ef4478eaf740d3c32c855e037ef3
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-01-17 14:21:18 +0900

    CommentモデルのFactoryBot,testの実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系" do
+    context "コメントが入力されている時" do
+      let(:comment){build(:comment)}
+      it "コメントが投稿できる" do
+        expect(comment).to be_valid
+      end
+    end
+  end
+
+  describe "異常系" do
+    context "コメントが空のとき" do
+      let(:comment){build(:comment,body: nil)}
+      it "投稿できない(can't be blank)" do
+      comment.valid?
+      expect(comment.errors.messages[:body]).to include "can't be blank"
+      end
+    end
+  end
 end
