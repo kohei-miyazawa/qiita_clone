49309c36d9a92fbc2255ade9cf02a962cf2320f9
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-01-18 22:39:42 +0900

    bundle exec rubocop -a実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'homes/index'
+  get "homes/index"
   root "homes#index"
-  
+
   namespace :api, format: "json" do
     namespace :v1 do
       resources :articles
