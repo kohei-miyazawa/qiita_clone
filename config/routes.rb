56bb59dec77fff48386eb6e7ff4e0e1b7a2b4001
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-01-19 12:54:34 +0900

    bundle exec rubocop -aの実行

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :articles
       mount_devise_token_auth_for "User", at: "auth", controllers: {
-        registrations: 'api/v1/auth/registrations'
+        registrations: "api/v1/auth/registrations",
       }
     end
   end
