b58611f5142c52ba6592293379b7fddbca001e76
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-01-20 11:58:56 +0900

    Add Navigation bar by vuetify

--- app/javascript/packs/app.ts
+++ app/javascript/packs/app.ts
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import Router from "./router/router";
 import Header from "./container/Header.vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+
+Vue.use(Vuetify);
 
 document.addEventListener("turbolinks:load", () => {
   new Vue({
