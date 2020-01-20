0156d72681f9e2f6a4a073ed388a68fefbbf2c73
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-01-20 14:27:52 +0900

    article.rbにenum追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
 
+  enum status: { draft: "draft", published: "published" }
+
   has_many :comments, dependent: :destroy
   has_many :article_likes, dependent: :destroy
   belongs_to :user
