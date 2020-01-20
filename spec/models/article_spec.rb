e3a2996c5ed0df911ca4ff8af3871376e67a0f48
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-01-20 14:37:27 +0900

    下書き記事のテストを実装

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,6 +8,22 @@ RSpec.describe Article, type: :model do
         expect(article).to be_valid
       end
     end
+
+    context "下書きとして投稿する場合" do
+      let(:article) { build(:article, status: "draft") }
+      it "下書き記事が投稿される" do
+        expect(article).to be_valid
+        expect(article.status).to eq "draft"
+      end
+    end
+
+    context "公開記事として投稿する場合" do
+      let(:article) { build(:article, status:　"published") }
+      it "公開記事が投稿される" do
+        expect(article).to be_valid
+        expect(article.status).to eq "published"
+      end
+    end
   end
 
   describe "異常系" do
