7f168df81b42e46645e2fff3c5f7203e29ff2b8e
kohei-miyazawa <kohei.miyazawa120@gmail.com>
2020-01-20 22:43:42 +0900

    draftsコントローラ修正(current_user)

--- app/controllers/api/v1/articles/drafts_controller.rb
+++ app/controllers/api/v1/articles/drafts_controller.rb
@@ -1,11 +1,12 @@
 class Api::V1::Articles::DraftsController < Api::V1::ApiController
+  before_action :authenticate_user!, only: [:index, :show]
   def index
-    articles = Article.draft
+    articles = current_user.articles.draft
     render json: articles
   end
 
   def show
-    article = Article.draft.find(params[:id])
+    article = current_user.articles.draft.find(params[:id])
     render json: article
   end
 end
